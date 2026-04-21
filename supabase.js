e3cc054a37e9540ed3bec7d4ddcb4878b6cf1fc5
Ccezzionale <tringali0511@gmail.com>
2026-04-21 10:20:13 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'https://TUO-PROGETTO.supabase.co'
-const supabaseKey = 'TUA_ANON_KEY'
+const supabaseUrl = 'https://vfzadnfpwsbzfiyzbpvx.supabase.co'
+const supabaseKey = 'sb_publishable_PL6nGBOyzAOiXWXjuplwYw_-fr60KdO'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
