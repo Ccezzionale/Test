01b1b17e6b0a25d715d82f5cc02ea0a188da2018
Ccezzionale <tringali0511@gmail.com>
2026-04-22 16:38:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,30 @@ self.addEventListener("install", () => {
 self.addEventListener("activate", () => {
   console.log("Service Worker attivo");
 });
+
+self.addEventListener("push", (event) => {
+  let data = {};
+  try {
+    data = event.data ? event.data.json() : {};
+  } catch {
+    data = {};
+  }
+
+  event.waitUntil(
+    self.registration.showNotification(data.title || "Lega degli Eroi", {
+      body: data.body || "È il tuo turno",
+      icon: "/Test/icon-192.png",
+      badge: "/Test/icon-192.png",
+      data: {
+        url: data.url || "/Test/"
+      }
+    })
+  );
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification?.data?.url || "/Test/")
+  );
+});
