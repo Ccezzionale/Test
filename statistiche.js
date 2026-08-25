d68a918262e7e4a372b7bfe0d8c092e95cb628e2
Ccezzionale <tringali0511@gmail.com>
2026-08-25 18:21:40 +0200

    Update statistiche.js

--- statistiche.js
+++ statistiche.js
@@ -1,6 +1,6 @@
 /********** CONFIG **********/
 const DEFAULT_CSV_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRhEJKfZhVb7V08KI29T_aPTR0hfx7ayIOlFjQn_v-fqgktImjXFg-QAEA6z7w5eyEh2B3w5KLpaRYz/pub?gid=1118969717&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSG3HrTJsfZGhgfJJx8l63QYhooGsyiydLf1OTt2JldOPx5nSZyJz00IplWA5YHGwjymNL9EXIVX5XA/pub?gid=1118969717&single=true&output=csv";
 
 // "" = tutte le fasi, altrimenti "Regular" o "Playoff"
 const PHASE_FILTER = "";
